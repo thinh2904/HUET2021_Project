a84f159864ba2d392d1d089062f83086feed11b9
thinh2904 <nguyentienthinh13a4hbt@gmail.com>
2021-01-12 14:50:20 +0700

    thịnh đã tải file này lên

---
+++ folder_op.py
@@ -0,0 +1,19 @@
+#Các thư viện cần thiết
+import os
+
+
+
+
+
+
+
+#Các hàm
+
+#Hàm này tao ten file tu dong bắt đầu bằng cụm filename tiếp theo là số các file kết thúc là .html
+def tao_ten_file_tu_dong():
+    pass
+
+
+#Hàm lưu nội dung vào file ở thư mục chỉ định
+def luu_noi_dung_file(content, folder):
+    pass
---
+++ web_op.py
@@ -0,0 +1,24 @@
+#Các thư viện cần thiết
+import requests
+from bs4 import BeautifulSoup
+
+#Các hàm cần thiết
+#Kết quả trả về là một văn bản dạng chuổi
+#Đọc nội dung của trang Web theo url chỉ định
+def doc_noi_dung(url):
+    pass
+
+#Hàm lấy các đường link web trong nội dung đọc về
+#Kết quả trả về là 1 list chứa các đường link
+def lay_cac_duong_link(content):
+    pass
+
+#Hàm kiểm tra tính hợp lệ của đường link
+#Kết quả trả về True nếu hợp lệ, False nếu không hợp lệ
+def kiem_tra_link():
+    pass
+
+#Hàm chỉnh sửa đường link nếu đường link là không đầy đủ
+#Kết quả trả về là một đường link đầy đủ
+def chinh_sua_link(link):
+    pass
